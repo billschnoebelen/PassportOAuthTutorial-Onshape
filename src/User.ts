328fa29dbdf48b140b21e736f87b0d3b778628c6
billschnoebelen <bill@circuitcase.com>
2022-11-05 17:07:36 -0700

    updated User

--- src/User.ts
+++ src/User.ts
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 
 const user = new mongoose.Schema({
-  googleId: {
-    required: false,
-    type: String,
-  },
-  twitterId: {
-    required: false,
-    type: String,
-  },
-  githubId: {
-    required: false,
-    type: String,
-  },
   onshapeId: {
-    required: false,
+    required: true,
     type: String,
   },
   username: {
     required: true,
     type: String,
   },
+  emails: [{
+    value: String
+  }],
+  accessToken: {
+    required: true,
+    type: String,
+  },
+  refreshToken: {
+    required: true,
+    type: String,
+  },
 });
+
+
+// onshapeId?: string,
+// username: string,
+// emails: Array<string>,
+// accessToken: string,
+// refreshToken: string,
+// __v: number,
+// _id: string
 
 
 export default mongoose.model("User", user)
